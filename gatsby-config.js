908bec7a8b7be0db921e1d9ff37a3d33fb1df3df
Samuel Pitoňák <sampittko@gmail.com>
2020-10-16 00:30:46 +0200

    Make changes inside package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
