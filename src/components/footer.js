9ee3e29822473aedfa5ceb0c029a5394d3c5e0cd
Samuel Pitoňák <sampittko@gmail.com>
2020-10-15 22:47:19 +0200

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,42 @@
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+
+const Footer = () => (
+  <div className="fixed bottom-0 py-12 px-4 w-screen">
+    <p className="mx-auto max-w-screen-xl sm:px-6 lg:px-8 lg:py-14 text-gray-400">
+      <StaticQuery
+        query={graphql`
+          query {
+            contentfulAuthor {
+              firstName
+              lastName
+              email
+            }
+            contentfulSite {
+              copyright
+            }
+          }
+        `}
+        render={data => {
+          const { firstName, lastName, email } = data.contentfulAuthor
+          const { copyright } = data.contentfulSite
+
+          return (
+            <>
+              &copy; {copyright}{" "}
+              <a
+                href={`mailto:${email}`}
+                title={email}
+                className="hover:text-gray-500 transition ease-in-out duration-150"
+              >
+                {firstName} {lastName}
+              </a>
+            </>
+          )
+        }}
+      />
+    </p>
+  </div>
+)
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "../../src/assets/css/main.css";
+import Footer from "./footer";
 
-const Layout = ({ children }) => <main>{children}</main>;
+const Layout = ({ children }) => (
+  <>
+    <main>{children}</main>
+    <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
